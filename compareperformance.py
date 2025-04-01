38322dd1dac050cd840a012cebb78b46cd81bcb5
Chris Bailey <baileybunch5dad@gmail.com>
2025-04-01 11:38:06 -0400

    add rmse

--- compareperformance.py
+++ compareperformance.py
@@ -1,4 +1,3 @@
-
 from SparseDist import SparseDist
 import numpy as np
 from DynamicDist import DynamicDist
@@ -6,49 +5,91 @@ from FixedArrayDist import FixedArrayDist
 from time import perf_counter
 import cProfile
 import matplotlib.pyplot as plt
+import inspect
 
-cumsum = {}
 
-def compare_to_numpy(rd, bigarray: np.array):
-    global cumsum
-    before = perf_counter()
-    for v in bigarray:
-        rd.add(v)
-    if isinstance(rd, DynamicDist):
-        hist, bins = rd.histogram(n_bins = 100)
-        # make hist and binse 'dense'
-        n_bins = 100
-        newBins = np.linspace(rd.bin_offset, rd.bin_offset + rd.bin_size * (n_bins+1), n_bins+1)
-        newHist = np.array([rd.bins.get(key, 0) for key in range(n_bins)])
-        hist, bins = newHist, newBins
-        # n_bins = int(np.ceil((np.max(bigarray) - np.min(bigarray))/rd.bin_size))
+#
+# Compute root mean square error 
+# based on distance from the upper left histogram corners
+# where the values are x=binstart, y=count
+#
+# and the algorithm (DynamicDist, SparseDist) computes 'predicted'
+# with numpy on all data computal 'actual'
+#
+def getRMSE(predHist : np.array, predBins : np.array, actHist : np.array, actBins : np.array):
+    predicted = np.transpose([predHist,predBins[:-1]])
+    actual = np.transpose([actHist,actBins[:-1]])
+    distances = np.sqrt(np.sum((predicted - actual)**2, axis=1))
+    # now we have all the distances, square them, sum them, didivde by n, and sqrt is rmse
+    squareerror = np.square(distances)
+    meansquareerror = squareerror.sum() / len(distances)
+    rmse = np.sqrt(meansquareerror)
+    return rmse
 
-    else:
-        hist, bins = rd.histogram()
-        n_bins = 100        
-    after = perf_counter()
-    elapsed = after - before
-    algorithm = type(rd).__name__
-    cumsum[algorithm] = cumsum.get(algorithm,0) + elapsed
-    nhist, nbins = np.histogram(bigarray, bins=n_bins)
-    histerrors = sum([abs(x-y) for x,y in zip(hist, nhist)])
-    print(elapsed, histerrors, histerrors/hist.sum(), type(rd).__name__)
-    plt.stairs(hist, bins, label=algorithm)
-    plt.stairs(nhist, nbins, label='numpy')
+#
+# Convert DynamicDist sparse bins and counts to match numpy dense bins and counts
+# 
+# e.g. asking for 100 bins in DynamicDist returns up to 100 bins, but possibly fewer
+#
+# DynamicDist internally after hisogram call in its sparse hashtable may have negative keys
+# and its bin_offset is not a "minimum" ( because of the keys )
+#
+# Multiple way to patch, one is get the true min, add offset, recompute keys in loop
+# Simplest is to use bins as data, and counts as weights, and call numpy histogram
+def convertDynamicDistToNumpy(dd, n_bins=100):
+    ddhist, ddbins = dd.histogram(n_bins = n_bins)
+    ddminval = ddbins[0]
+    ddbinsize = dd.bin_size
+    ddmaxval = ddbins[-1] + ddbinsize
+    newbins = np.linspace(ddminval, ddmaxval, n_bins+1)
+    midpoints = ddbins + ddbinsize/2
+    ddhist, ddbins = np.histogram(midpoints, weights=ddhist, bins=newbins)
+    return ddhist, ddbins
+ 
+#
+# Overlay the hisogram of the predictor versus the actual
+#
+def graphit(testname: str, name: str, elapsed: float, rmse: float,
+            predhist: np.array, predbins: np.array,
+            acthist: np.array, actbins: np.array):
+    plt.stairs(acthist, actbins, label='numpy')
+    plt.stairs(predhist, predbins, label=name)
     plt.legend()
+    plt.title(f'{testname} {name} Time={elapsed:.2f} RMSE={rmse:.2f}')
     plt.show()
-    return hist, bins, nhist, nbins
-    
-def test_random_uniform(rd):
-    vals = np.random.uniform(low=0, high=100, size=100000000)
-    return compare_to_numpy(rd, vals)
+     
+#
+# Compare an algorithm that does not hold the entire data and must 'estimate'
+# an a given data set by refining a sample to something that hold the entire data
+#    
+def compare_to_numpy(bigarray: np.array):
+    testname = inspect.stack()[1].function
+    actualhist, actualbins = np.histogram(bigarray, bins=100)
+    for algorithm in [DynamicDist(), SparseDist()]:
+        name = type(algorithm).__name__
+        before = perf_counter()
+        for v in bigarray: 
+            algorithm.add(v)
+        if isinstance(algorithm,DynamicDist):
+            predhist, predbins = convertDynamicDistToNumpy(algorithm, n_bins=100)
+        else:
+            predhist, predbins = algorithm.histogram()
+        # print(f'{actualhist.sum()=} {predhist.sum()=}')
+        elapsed = perf_counter() - before
+        rmse = getRMSE(predhist, predbins, actualhist, actualbins)
+        print(f'{testname} {name} Time={elapsed:.2f} RMSE={rmse:.2f}')
+        graphit(testname,name,elapsed,rmse,predhist, predbins, actualhist, actualbins)
 
-def test_random_uniform_monotonic(rd):
+def test_random_uniform():
+    vals = np.random.uniform(low=0, high=100, size=100000000)
+    return compare_to_numpy(vals)
+
+def test_random_uniform_monotonic():
     vals = np.random.uniform(low=0, high=100, size=100000000)
     vals = np.sort(vals)
-    return compare_to_numpy(rd, vals)
+    return compare_to_numpy(vals)
 
-def test_sparse_multiple_groups(rd):
+def test_sparse_multiple_groups():
     sigma = 5
     bigarray = np.empty((0))
     for mu in range(27,48,10):
@@ -58,44 +99,13 @@ def test_sparse_multiple_groups(rd):
     for mu in range(4000,40000,200): # add to the right
         vals = np.random.normal(loc=mu, scale=sigma, size=1000000)
         bigarray = np.concatenate((bigarray, vals), axis=0)
-    return compare_to_numpy(rd, bigarray)
-
+    return compare_to_numpy(bigarray)
 
 def Main():
-    sigma = 5
-    bigarray = np.empty((0))
-    for mu in range(27,48,10):
-        vals = np.random.normal(loc=mu, scale=sigma, size=1000000)
-        bigarray = np.concatenate((bigarray, vals), axis=0)
-    sigma = 300
-    for mu in range(4000,40000,2000): # add to the right
-        vals = np.random.normal(loc=mu, scale=sigma, size=1000000)
-        bigarray = np.concatenate((bigarray, vals), axis=0)
-    dd = DynamicDist()
-    sd = SparseDist()
-    for v in bigarray:
-        dd.add(v)
-        sd.add(v)
-    ddhist, ddbins = dd.histogram(n_bins = 100)
-    n_bins = 100
-    ddbins = np.linspace(dd.bin_offset, dd.bin_offset + dd.bin_size * (n_bins+1), n_bins+1)
-    ddhist = np.array([dd.bins.get(key, 0) for key in range(n_bins)])
-    sdhist, sdbins = sd.histogram()
-    nphist, npbins = np.histogram(bigarray, bins=n_bins)
-    plt.stairs(ddhist, ddbins, label='DynamicDist')
-    plt.stairs(sdhist, sdbins, label='SparseDist')
-    plt.stairs(nphist, npbins, label='numpy')
-    plt.legend()
-    plt.show()
-
-
-def Main2():
     for i in range(3):
         for whichTest in [test_random_uniform, test_random_uniform_monotonic, test_sparse_multiple_groups]:
             print(whichTest.__name__)
-            for rd in [DynamicDist(), SparseDist()]: # FixedDist()
-                hist, bins, nhist, nbins = whichTest(rd)
-
+            whichTest()
 
 if __name__ == "__main__":
     # cProfile.run('Main()')
